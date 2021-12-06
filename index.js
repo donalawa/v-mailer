78439651899426e69527d643cef2829439c2ff6b
donalawa <awadonalcien12@gmail.com>
2021-12-05 19:34:45 -0800

    update

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+require('dotenv').config();
+const mailer = require('nodemailer');
+const cors = require('cors')
+
+
+const app = express();
+
+app.use(express.json())
+
+app.use(cors('*'))
+
+app.get('/', (req, res) => {
+    res.send({ message: 'This is working' })
+})
+
+
+
+app.post('/', (req, res, next) => {
+    console.log(req.body)
+    let senderEmail = req.body.senderEmail;
+    // Thi is 
+    let name = req.body.name;
+    let address = req.body.address;
+    let puppyName = req.body.puppyName;
+    let phone = req.body.phone;
+    let email = req.body.email;
+    let message = req.body.message;
+    // This is a change
+    //Test
+    let body = {
+        from: `${email}`,
+        to: senderEmail,
+        subject: `${name}`,
+        html: `<div><h3 style="color: green; text-align: center">New Contact Submited</h3><p> <span style='font-weight: bold'> FirstName: </span>  ${name} <br/> <span style='font-weight: bold'>Address:</span>  ${address} <br/> <span style='font-weight: bold'>Phone:</span>  ${phone} <br/> <span style='font-weight: bold'>PuppyName:</span>  ${puppyName} <br/> <span style='font-weight: bold'>Email:</span>  ${email} <br/> <span style='font-weight: bold'>Message:</span> ${message}</p></div>`
+    }
+
+    const transporter = mailer.createTransport({
+        secure: true,
+        host: 'smtp.gmail.com',
+        port: 465,
+        auth: {
+            user: 'mymailer237@gmail.com',
+            pass: 'icui4cumise7'
+        }
+    })
+
+
+    transporter.sendMail(body, (err, result) => {
+        console.log('Sending Mail')
+        if (err) {
+            console.log(err);
+            res.status(401).send({ message: 'There was an error' })
+            return false
+        }
+        console.log(result);
+        console.log("email sent");
+        res.send({ message: 'Hello World Working' })
+
+    })
+})
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log('Server Running')
+})
