45adbfb4d24affe3365a9e0ef1cca0b8ce2cd625
donalawa <awadonalcien12@gmail.com>
2022-03-11 17:21:12 +0100

    update

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/', (req, res, next) => {
         port: 465,
         auth: {
             user: 'mymailer2377@gmail.com',
-            pass: 'icui4cumiSE$$7'
+            pass: 'icui4cumISE$$7'
         }
     })
 
